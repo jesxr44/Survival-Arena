d06639091e71fcafdf60f15c97d317cfee2e5dba
Jesus Escribano <jesus.escribano@paradoxinteractive.com>
2022-10-03 11:41:58 +0200

    Jump

---
+++ Assets/Scripts/CharacterJump.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterJump : MonoBehaviour
+{
+    private CharacterController characterController;
+
+    private bool isGrounded = false;
+    [SerializeField]
+    private float jumpHeight = 5.0f;
+    private bool jumpPressed = false;
+    private bool isJumping = false;
+    [SerializeField]
+    private float jumpTime;
+    private float jumpTimeCounter;
+
+
+    [SerializeField]
+    private float gravity = -9.81f;
+
+    [SerializeField]
+    private Transform groundCheck;
+    [SerializeField]
+    LayerMask groundLayer;
+    private Vector3 velocity;
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
+        if (isGrounded && velocity.y <= 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (jumpPressed)
+        {
+            if (isGrounded)
+            {
+                jumpTimeCounter = jumpTime;
+                //velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                velocity.y = jumpHeight;
+            }
+            jumpPressed = false;
+        }
+
+        if (isJumping)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                velocity.y = jumpHeight;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Debug.Log("Jump");
+            jumpPressed = true;
+            isJumping = true;
+        }
+        if (context.canceled)
+        {
+            Debug.Log("Jump Cancelled");
+            isJumping = false;
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(PlayerInput))]
+[RequireComponent(typeof(Animator))]
+public class CharacterMove : MonoBehaviour
+{
+    private const string IsRunning = "isRunning";
+
+    private CharacterController characterController;
+    private Animator animator;
+
+    private Vector2 currentMovementInput;
+    private Vector3 currentMovement;
+    private bool isMovementPressed;
+
+    // Constants
+    [SerializeField]
+    private float rotationFactorPerFrame = 1.0f;
+    [SerializeField]
+    float runMultiplier = 3.0f;
+    bool isJumpPressed = false;
+
+    int isRunningHash;
+
+    //gravity variables
+    float gravity = -9.8f;
+    float groundedGravity = -0.5f;
+
+    // jumpint variables
+    float initialJumpVelocity;
+    float maxJumpHeight = 1.0f;
+    float maxJumpTime = 0.5f;
+    bool isJumping = false;
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        isRunningHash = Animator.StringToHash(IsRunning);
+
+        setupJumpVariables();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        HandleRotation();
+        HandleAnimation();
+        characterController.Move(currentMovement * Time.deltaTime);
+        HandleGravity();
+        HandleJump();
+    }
+    void setupJumpVariables()
+    {
+        float timeToApex = maxJumpTime / 2;
+        gravity = (-2 * maxJumpHeight) / Mathf.Pow(timeToApex, 2);
+        initialJumpVelocity = (2 * maxJumpHeight) / timeToApex;
+    }
+
+    void HandleGravity()
+    {
+        if (characterController.isGrounded)
+        {
+            currentMovement.y = groundedGravity;
+        }
+        else
+        {
+            currentMovement.y += gravity * Time.deltaTime;
+        }
+    }
+
+    void HandleAnimation()
+    {
+        bool isRunning = animator.GetBool(isRunningHash);
+
+        if (isMovementPressed && !isRunning)
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+        else if (!isMovementPressed && isRunning)
+        {
+            animator.SetBool(isRunningHash, false);
+        }
+    }
+
+    void HandleRotation()
+    {
+        Vector3 positionToLookAt = new Vector3();
+        positionToLookAt.x = currentMovement.x;
+
+        Quaternion currentRotation = transform.rotation;
+        if (isMovementPressed)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(positionToLookAt);
+            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame * Time.deltaTime);
+        }
+
+    }
+
+    void HandleJump()
+    {
+        if (!isJumping && characterController.isGrounded && isJumpPressed)
+        {
+            isJumping = true;
+            currentMovement.y = initialJumpVelocity;
+        } else if (!isJumpPressed && isJumping && characterController.isGrounded)
+        {
+            isJumping = false;
+        }
+    }
+
+    public void Movement(InputAction.CallbackContext context)
+    {
+        currentMovementInput = context.ReadValue<Vector2>();
+        currentMovement = new Vector3(currentMovementInput.x * runMultiplier, 0, 0);
+        isMovementPressed = currentMovement.x != 0;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        isJumpPressed = context.ReadValueAsButton();
+        Debug.Log(isJumpPressed);
+    }
+}
